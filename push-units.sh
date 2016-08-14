e6f709bdf66a1399fb41e355352f87eb7b129969
Chad Autry <chad.autry@gmail.com>
2016-08-14 15:27:19 -0500

    Seems there needs to be 2 commands to add everything and remove anything missing from git

--- push-units.sh
+++ push-units.sh
@@ -3,6 +3,7 @@ set -ev;
 git init
 git config user.name ${GH_NAME}
 git config user.email ${GH_EMAIL}
+git add .
 git add -u .
 git commit -a -m "Automated push of generated files  [ci skip]"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}"  master > /dev/null 2>&1
