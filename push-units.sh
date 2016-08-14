4b2190f370cb3afc5535a61bcaeceb5be41b3fcd
Chad Autry <chad.autry@gmail.com>
2016-08-14 17:06:27 -0500

    TravisCI is detached, need to specify src ref in push command

--- push-units.sh
+++ push-units.sh
@@ -6,4 +6,4 @@ git config user.email ${GH_EMAIL}
 git add .
 git add -u .
 git commit -a -m "Automated push of generated files  [ci skip]"
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" HEAD:master > /dev/null 2>&1
