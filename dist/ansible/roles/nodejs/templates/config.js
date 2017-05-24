417fa36cbdfef39df26d2c42ada4fc5e92fccb51
chad-autry <chad.autry@gmail.com>
2017-05-24 12:18:44 +0000

    Automated push of generated files  [ci skip]

--- dist/ansible/roles/nodejs/templates/config.js
+++ dist/ansible/roles/nodejs/templates/config.js
@@ -1,12 +1,6 @@
 module.exports = {
-  // App Settings
-  TOKEN_SECRET: '{{jwt_token_secret}}',
   PORT: 80,
-
-  // OAuth 2.0
-  //TODO Make these parameters more generic. Just write out all params that exist at etcd path
-  // Google
-  GOOGLE_CLIENT_ID: '{{google_client_id}}',
-  GOOGLE_REDIRECT_URI: '{{google_redirect_uri}}',
-  GOOGLE_SECRET: '{{google_auth_secret}}'
+{%- for key in node_config[identifier] %}
+  {{key}}: '{{node_config[identifier][key]}}'{% if not loop.last %},{% endif %}
+{%- endfor %}
 };
